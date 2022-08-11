1b714b341e1ecddb5974bda0aa195d48e5e4ea46
JackssonYuri <78384230+JackssonYuri@users.noreply.github.com>
2022-08-11 09:52:10 -0300

    Update UserController.js

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,10 +1,32 @@
-class IClienteController{
+const mongoose = require('mongoose');
+const Cliente = require('../Controller/Cliente');
 
-   constructor(){}
-    index(){}
-    show(){}
-    store(){}
-    destroy(){}
-    update(){}
-}
-module.exports = IClienteController;
+class ClienteController extends IClienteController{
+  constructor(){
+    super();
+
+  // cria cliente
+  async store(req, res)
+     {
+        const cliente =  await Cliente.create(req.body);
+
+        return res.json(cliente);
+     },
+   
+  //busca cliente
+  async index(req,res)
+    {
+        let cliente = await Cliente.find(
+          { nome : req.query.nome},
+          { CPF : req.query.CPF}  );
+        return res.json(cliente);
+    }
+
+  // altera cliente
+  // devesse passar três dados: o nome e o cpf via param e o json via body
+   async update(req,res){
+        let cliente = await User.findByIdAndUpdate(req.params.nome,req.params.cpf,req.body,{new:true}); 
+        return res.json(cliente);
+    }
+ 
+};
