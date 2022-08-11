5cbb9ba5d36e660ea295975f2551f095758d6e1e
JackssonYuri <78384230+JackssonYuri@users.noreply.github.com>
2022-08-11 09:00:17 -0300

    Update TestController.js

--- controllers/TestController.js
+++ controllers/TestController.js
@@ -1,1 +1,10 @@
-eai
+// index, show, store, udpdate , destroy
+module.exports = {
+     async show(req, res)
+    {
+      const users = [
+       {name: 'Jones', email: 'jones@gmail.com'}
+      ]
+        return res.json(users);
+    }
+};
